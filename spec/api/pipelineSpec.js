d39914cb096e7096d631109f52e806d5ca20cdf1
Vladimir Yazykov <neizerth@gmail.com>
2019-01-29 17:15:03 +0300

    updated pipelines spec

--- spec/api/pipelineSpec.js
+++ spec/api/pipelineSpec.js
@@ -1,6 +1,5 @@
 import config from '../support/config';
 import AmoCRM from '../../src/AmoCRM';
-import Pipeline from "../../src/api/activeRecords/Pipeline";
 
 let client;
 
@@ -32,8 +31,32 @@ describe( 'AmoCRM API Pipeline Interface', () => {
       .then( pipeline => {
         expect( pipeline.name ).toBe( 'Test Pipeline' );
         done();
-      })
+      });
+  });
 
+  it( 'creates and updates pipeline', done => {
+    const pipeline = new client.Pipeline({
+      name: 'Test Pipeline',
+      statuses: [
+        {
+          color: '#fffeb2',
+          name: 'Test status'
+        }
+      ]
+    });
+
+    pipeline.save()
+      .then( pipeline => {
+        expect( pipeline.id ).toBeDefined();
+      })
+      .then(() => client.Pipeline.findById( pipeline.id ))
+      .then( pipeline => pipeline.save({
+        name: 'Updated Pipeline'
+      }))
+      .then( pipeline => {
+        expect( pipeline.name ).toBe( 'Updated Pipeline' );
+        done();
+      });
   });
 
   it( 'removes all pipelines', done => {
@@ -44,9 +67,7 @@ describe( 'AmoCRM API Pipeline Interface', () => {
 
         return client.Pipeline.remove( ids );
       })
-      .then( response => {
-        done();
-      });
+      .then( done );
 
   });
 
