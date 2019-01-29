fa4e1dbdc4cdb354053149635c2e8542bb9c65e2
Vladimir Yazykov <neizerth@gmail.com>
2019-01-29 18:07:04 +0300

    clean up code

--- src/api/activeRecords/Lead.js
+++ src/api/activeRecords/Lead.js
@@ -1,9 +1,9 @@
 'use strict';
 
 import Entity from '../../base/activeRecords/EntityActiveRecord';
-import Removable from "../../base/activeRecords/behaviors/Removable";
-import Notable from "../../base/activeRecords/behaviors/Notable";
-import Taskable from "../../base/activeRecords/behaviors/Taskable";
+import Removable from '../../base/activeRecords/behaviors/Removable';
+import Notable from '../../base/activeRecords/behaviors/Notable';
+import Taskable from '../../base/activeRecords/behaviors/Taskable';
 
 class Lead extends Entity {
   static behaviors = [ new Removable, new Notable, new Taskable ];
