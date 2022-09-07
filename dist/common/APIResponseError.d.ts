c3ad90810ec6f00a00b99e47ffbb01cefa1e062d
Vladimir Yazykov <neizerth@gmail.com>
2022-09-07 20:28:43 +0300

    build / docs

--- dist/common/APIResponseError.d.ts
+++ dist/common/APIResponseError.d.ts
@@ -3,7 +3,7 @@ import { IAPIResponse } from "../interfaces/common";
  * Класс ошибки API портала
  * */
 export default class APIResponseError<T> extends Error {
-    readonly apiResponse: T;
+    readonly data: T;
     readonly response: IAPIResponse<string>;
-    constructor(message: string, apiResponse: T, response: IAPIResponse<string>);
+    constructor(message: string, data: T, response: IAPIResponse<string>);
 }
--- dist/common/APIResponseError.js
+++ dist/common/APIResponseError.js
@@ -4,9 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * Класс ошибки API портала
  * */
 class APIResponseError extends Error {
-    constructor(message, apiResponse, response) {
+    constructor(message, data, response) {
         super(message);
-        this.apiResponse = apiResponse;
+        this.data = data;
         this.response = response;
     }
 }
